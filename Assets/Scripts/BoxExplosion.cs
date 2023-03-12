88256886ddf4349c51c542743816f51245eff847
Bertin <bertin@gmail.com>
2023-03-12 12:48:40 +0100

    Initial commit for Box.

---
+++ Assets/Scripts/BoxExplosion.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BoxExplosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitBox();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Explosion();
+    }
+
+    public void InitBox()
+    {
+        // Box internal initialization.
+    }
+
+    public void Explosion()
+    {
+        // Explodes when certain condition is met.
+    }
+}
