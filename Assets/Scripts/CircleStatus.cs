c429c9caba5e19a5ccf618215e08f3598f9f4ea0
Alberto <alberto@gmail.com>
2023-03-12 12:09:39 +0100

    Funcionalidad inicial para el Circle prefab finalizada.

---
+++ Assets/Scripts/CircleStatus.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CircleStatus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitCircle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCircle();
+    }
+
+    public void InitCircle()
+    {
+        // Tareas de inicialización internas del círculo.
+    }
+
+    public void MoveCircle()
+    {
+        // Mueve el círculo conforme pase el tiempo.
+    }
+}
