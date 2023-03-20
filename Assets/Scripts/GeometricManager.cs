ab6933037299aa6a123a3eea702da683892ba9e5
Alberto <alberto@gmail.com>
2023-03-20 08:14:36 +0100

    Circle functionality for GeometricManager.

---
+++ Assets/Scripts/GeometricManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GeometricManager : MonoBehaviour
+    {
+        public static void InteractWithCircle(CircleStatus circle)
+        {
+            // Get a circle reference and interact with it.
+        }
+    }
+}
