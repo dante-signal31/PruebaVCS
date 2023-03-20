ac4acd2c7b4ed099a99d383ba0dc3917d54ab776
Bertin <bertin@gmail.com>
2023-03-20 07:54:16 +0100

    GeometricManager initialization and Box interaction done.

---
+++ Assets/Scripts/GeometricManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GeometricManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // Get references to other components.
+        }
+
+        private void Start()
+        {
+            // Initialize using other components values.
+        }
+
+        public static void InteractWithBox(BoxExplosion box)
+        {
+            // Get a box reference and interact with it.
+        }
+    }
+}
